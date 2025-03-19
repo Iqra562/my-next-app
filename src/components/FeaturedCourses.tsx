1d89c11dccd3a3090a194896b0e2e44057a64565
Iqra Khaliq <114660239+Iqra562@users.noreply.github.com>
2025-03-19 23:46:44 +0500

    featured card ready

--- src/components/FeaturedCourses.tsx
+++ src/components/FeaturedCourses.tsx
@@ -1,6 +1,21 @@
+'use client'
+
 import Link from 'next/link';
 import  courseData from '../data/music_courses.json'
+import { BackgroundGradient } from './background-gradient';
+
+interface Course{
+  id: number,
+  title: string,
+  slug: string,
+  description:string,
+  price: number,
+  instructor:string,
+  isFeatured: boolean,
+}
+
 function FeaturedCourses(){
+ const featuredCourses = courseData.courses.filter((courses : Course ) =>courses.isFeatured)
     return(
   <div className='py-12 bg-gray-900'>
     <div>  <div className="text-center">
@@ -9,6 +24,22 @@ function FeaturedCourses(){
             </div></div>
             <div className="mt-10 mx-8">
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 justify-center">
+              {
+                featuredCourses.map((course :Course,index)=>(
+                <div key={course.id} className='flex justify-center'>
+                 <BackgroundGradient
+                        className="flex flex-col rounded-[22px] bg-black dark:bg-zinc-900 overflow-hidden h-full max-w-sm">
+                            <div className="p-4 sm:p-6 flex flex-col items-center text-center flex-grow">
+                                <p className="text-lg sm:text-xl text-white mt-4 mb-2 dark:text-neutral-200">{course.title}</p>
+                                <p className="text-sm text-neutral-600 dark:text-white flex-grow">{course.description}</p>
+                                <Link href={`/courses/${course.slug}`} className='dark:text-gray-300'>
+                                Learn More
+                                </Link>
+                            </div>
+                        </BackgroundGradient>
+                </div>
+                ))
+              }
  </div>
  </div>
     <div className="mt-20 text-center">
