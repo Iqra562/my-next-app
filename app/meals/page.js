f7606e25a8f064f2f32db3be4d199da3deb820ea
Iqra Khaliq <114660239+Iqra562@users.noreply.github.com>
2025-03-25 03:45:22 -0700

    update

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,14 +1,22 @@
 import Link from "next/link";
-
+import classes from './page.module.css'
+import MealsGrid from "@/components/meals/meals-grid";
 export default function MealsPage() {
     return (
-      <main>
-        <h1 style={{ color: 'white', textAlign: 'center' }}>
-         Meals 
-        </h1>
-        {/* <Link href='meal-1'>MEAL 1</Link> */}
-
-      </main>
+      <>
+    <header className={classes.header}>
+   <h1>Delicious meals, created <span className={classes.highlight}>by you</span> </h1>
+   <p>Choose you fvrt recipe and cook it yourself</p>
+   <p className={classes.cta}>
+ <Link href="/meals/share">
+ Share your favorite recipe
+ </Link>
+   </p>
+    </header>
+    <main className={classes.main}>
+ <MealsGrid meals={[]}/>
+    </main>
+      </>
     );
   }
   
