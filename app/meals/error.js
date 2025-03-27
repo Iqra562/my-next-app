002d6661798aaf91d9f76b9c044a45bf3c560d38
Iqra Khaliq <114660239+Iqra562@users.noreply.github.com>
2025-03-28 01:08:29 +0500

    handle error

---
+++ app/meals/error.js
@@ -0,0 +1,14 @@
+'use client'
+export default function Error(){
+    return(
+        <main className="error">
+            <h1>
+                An error occured!
+            </h1>
+            <p>
+                failed to fetch meal data.
+            </p>
+
+        </main>
+    )
+}
